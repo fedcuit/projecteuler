dce9d46732091141b0b222d111629b6c268315a2
Erdong Feng <edfeng@thoughtworks.com>
2014-05-11 14:20:37 +0800

    move isPrime and factors to a Utils class.

--- src/main/scala/Problem3.scala
+++ src/main/scala/Problem3.scala
@@ -1,13 +1,7 @@
-import scala.math.sqrt
+import Utils._
 
 object Problem3 {
   def largestPrime(n: Long): Long = {
     factors(600851475143L).filter(isPrime).max
-  }
-
-  private def isPrime(l: Long): Boolean = factors(l).toList.sorted == List(1L, l)
-
-  private def factors(n: Long) = {
-    (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet
   }
 }
---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,7 @@
+import scala.math._
+
+object Utils {
+  def factors(n: Long) = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
+
+  def isPrime(l: Long): Boolean = factors(l).sorted == List(1, l)
+}
