b5dc2f7539f25092eadf34ee38be9de0136f9f00
Erdong Feng <edfeng@thoughtworks.com>
2014-05-15 12:47:23 +0800

    add method to calculate factorial of a number.

--- src/main/scala/MathUtils.scala
+++ src/main/scala/MathUtils.scala
@@ -1,6 +1,8 @@
 import scala.math._
 
 object MathUtils {
+  def factorial(n: Int): Int = if (n == 1) 1 else n * factorial(n - 1)
+
   def factors(n: Long): List[Long] = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
 
   def isPrime(l: Long): Boolean = factors(l).sorted == List(1, l)
--- src/test/scala/MathUtils$Test.scala
+++ src/test/scala/MathUtils$Test.scala
@@ -12,5 +12,9 @@ class MathUtils$Test extends FunSpec with ShouldMatchers {
     it("should able to find our all factors of a number") {
       MathUtils.factors(28).sorted should equal(List(1, 2, 4, 7, 14, 28))
     }
+
+    it("should able to calculate n's factorial") {
+      MathUtils.factorial(10) should equal(3628800)
+    }
   }
 }
