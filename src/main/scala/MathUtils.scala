becd9fce4317d00bcbb5b647e58cceb583a96b0c
Erdong Feng <edfeng@thoughtworks.com>
2014-05-15 13:37:41 +0800

    add method to generate a amicablePair.

--- src/main/scala/MathUtils.scala
+++ src/main/scala/MathUtils.scala
@@ -1,9 +1,14 @@
 import scala.math._
 
 object MathUtils {
+  def amicablePair(n: Long): (Long, Long) = {
+    val sum = factors(n).init.sum
+    (sum, factors(sum).init.sum)
+  }
+
   def factorial(n: BigInt): BigInt = if (n == BigInt(1)) 1 else n * factorial(n - 1)
 
-  def factors(n: Long): List[Long] = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
+  def factors(n: Long): List[Long] = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList.sorted
 
-  def isPrime(l: Long): Boolean = factors(l).sorted == List(1, l)
+  def isPrime(l: Long): Boolean = factors(l) == List(1, l)
 }
--- src/test/scala/MathUtils$Test.scala
+++ src/test/scala/MathUtils$Test.scala
@@ -10,11 +10,15 @@ class MathUtils$Test extends FunSpec with ShouldMatchers {
     }
 
     it("should able to find our all factors of a number") {
-      MathUtils.factors(28).sorted should equal(List(1, 2, 4, 7, 14, 28))
+      MathUtils.factors(28) should equal(List(1, 2, 4, 7, 14, 28))
     }
 
     it("should able to calculate n's factorial") {
       MathUtils.factorial(10) should equal(3628800)
+    }
+
+    it("should able to generate a possible amicable numbers pair") {
+      MathUtils.amicablePair(220) should equal((284, 220))
     }
   }
 }
