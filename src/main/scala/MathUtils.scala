0ebc384917168e96316c49331f066594df234215
Erdong Feng <edfeng@thoughtworks.com>
2014-05-15 12:41:05 +0800

    Add test for MathUtils.

--- src/main/scala/MathUtils.scala
+++ src/main/scala/MathUtils.scala
@@ -1,7 +1,7 @@
 import scala.math._
 
 object MathUtils {
-  def factors(n: Long) = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
+  def factors(n: Long): List[Long] = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
 
   def isPrime(l: Long): Boolean = factors(l).sorted == List(1, l)
 }
---
+++ src/test/scala/MathUtils$Test.scala
@@ -0,0 +1,16 @@
+import org.scalatest.{ShouldMatchers, FunSpec}
+
+class MathUtils$Test extends FunSpec with ShouldMatchers {
+  describe("MathUtils") {
+    it("should able to tell whether a number is prime") {
+      MathUtils.isPrime(13) should equal(true)
+      MathUtils.isPrime(7) should equal(true)
+      MathUtils.isPrime(1) should equal(false)
+      MathUtils.isPrime(39) should equal(false)
+    }
+
+    it("should able to find our all factors of a number") {
+      MathUtils.factors(28).sorted should equal(List(1, 2, 4, 7, 14, 28))
+    }
+  }
+}
