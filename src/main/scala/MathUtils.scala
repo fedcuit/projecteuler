f28351d5bbf2c44bbbac8f567039b3b37f530045
Erdong Feng <edfeng@thoughtworks.com>
2014-05-15 13:00:01 +0800

    Change number type to BigInt since the result could be very large.

--- src/main/scala/MathUtils.scala
+++ src/main/scala/MathUtils.scala
@@ -1,7 +1,7 @@
 import scala.math._
 
 object MathUtils {
-  def factorial(n: Int): Int = if (n == 1) 1 else n * factorial(n - 1)
+  def factorial(n: BigInt): BigInt = if (n == BigInt(1)) 1 else n * factorial(n - 1)
 
   def factors(n: Long): List[Long] = (for (i <- 1L to (sqrt(n).toInt + 1) if n % i == 0) yield List(i, n / i)).flatten.toSet.toList
 
