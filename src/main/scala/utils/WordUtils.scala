3934df88ac5553537df140c2219d01fda87802fe
Erdong Feng <edfeng@thoughtworks.com>
2014-05-19 17:29:54 +0800

    util method to find the longestRecurringCycle in a string.

--- src/main/scala/utils/WordUtils.scala
+++ src/main/scala/utils/WordUtils.scala
@@ -1,6 +1,14 @@
 package utils
 
 object WordUtils {
+  def continuousCount(s: String, sub: String): Int = {
+    val r = """(\d*?)(%s){2,}(\d*?)""".format(sub).r
+    s match {
+      case r(prefix, cycle, suffix) => (s.length - prefix.length - suffix.length) / sub.length
+      case _ => 0
+    }
+  }
+
   def beHead(s: String): List[String] = {
     val letters = s.split("").tail.toList
     s :: (1 to s.length - 1).map((i: Int) => {
@@ -10,4 +18,12 @@ object WordUtils {
   }
 
   def position(c: Char): Int = ('a'.toInt to 'z'.toInt).indexWhere(_ == c.toLower.toInt) + 1
+
+  def longestRecurringCycle(s: String): String = {
+    val digits = s.split("").tail
+    (digits.size - 2 to digits.size / 2 by -1).map(i => {
+      val tail= digits.drop(i).distinct.mkString("")
+      (tail, continuousCount(s, tail))
+    }).maxBy(_._2)._1
+  }
 }
--- src/test/scala/utils/WordUtils$Test.scala
+++ src/test/scala/utils/WordUtils$Test.scala
@@ -1,27 +1,42 @@
 package utils
 
 import org.scalatest.{ShouldMatchers, FunSpec}
+import utils.WordUtils.{position, beHead, longestRecurringCycle, continuousCount}
 
 class WordUtils$Test extends FunSpec with ShouldMatchers {
   describe("WordUtils") {
     it("should able to figure out the position of a lower case letter in letter table") {
-      WordUtils.position('c') should equal(3)
-      WordUtils.position('o') should equal(15)
-      WordUtils.position('l') should equal(12)
-      WordUtils.position('i') should equal(9)
-      WordUtils.position('n') should equal(14)
+      position('c') should equal(3)
+      position('o') should equal(15)
+      position('l') should equal(12)
+      position('i') should equal(9)
+      position('n') should equal(14)
     }
 
     it("should able to figure out the position of a upper case letter in letter table") {
-      WordUtils.position('C') should equal(3)
-      WordUtils.position('O') should equal(15)
-      WordUtils.position('L') should equal(12)
-      WordUtils.position('I') should equal(9)
-      WordUtils.position('N') should equal(14)
+      position('C') should equal(3)
+      position('O') should equal(15)
+      position('L') should equal(12)
+      position('I') should equal(9)
+      position('N') should equal(14)
     }
     
     it("should able to generate a sequence heads with a letter and tails with a ordered sequence") {
-      WordUtils.beHead("abcde") should equal(List("abcde", "bacde", "cabde", "dabce", "eabcd"))
+      beHead("abcde") should equal(List("abcde", "bacde", "cabde", "dabce", "eabcd"))
+    }
+
+    it("should able to find the longest recurring cycle in a string") {
+      longestRecurringCycle("166666666666666666666666666666666") should equal("6")
+      longestRecurringCycle("333333333333333333333333333333333") should equal("3")
+      longestRecurringCycle("142857142857142857142857142857142857142857") should equal("142857")
+    }
+
+    it("should able to find out the continuous occurrence count of a sub string in a string") {
+      continuousCount("16126126126126129", "612") should equal(5) // with both prefix and suffix
+      continuousCount("1612612612612612", "612") should equal(5) // with only prefix
+      continuousCount("6126126126126129", "612") should equal(5) // with only suffix
+      continuousCount("612612612612612", "612") should equal(5) // without prefix and suffix
+      continuousCount("16126126126126129", "4") should equal(0)
     }
   }
 }
